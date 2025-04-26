4869a1d43053cd4ef58ba7c7a96d1e9d1e06ad09
naveenkumar58 <mithaninaveen246@gmail.com>
2025-04-26 16:44:49 +0200

    Project setup, Header and Card styling

---
+++ src/components/card/card.tsx
@@ -0,0 +1,13 @@
+import './card.css'
+
+const Card=()=>{
+    return(
+        <div className="card">
+          <h3>My User</h3>
+          <p>User@gmail.com</p>
+          <span>company: conceptboard</span>
+        </div>
+    )
+}
+
+export default Card
